f6d2031be26eacaeff65cc577546c1452050c98a
John Orthoefer <john@orthoefer.org>
2021-09-26 23:39:43 -0400

    simple grab the HTML

---
+++ src/grabscreen/main.go
@@ -0,0 +1,101 @@
+// Command screenshot is a chromedp example demonstrating how to take a
+// screenshot of a specific element and of the entire browser viewport.
+package main
+
+import (
+   "os"
+	"context"
+	"io/ioutil"
+	"log"
+	"github.com/chromedp/chromedp"
+   "github.com/chromedp/cdproto/dom" 
+)
+
+func main() {
+	opts := []chromedp.ExecAllocatorOption{
+		chromedp.UserAgent( "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:59.0) Gecko/20100101 Firefox/59.0"),
+		chromedp.WindowSize(1920, 1080),
+		chromedp.NoFirstRun,
+		chromedp.NoDefaultBrowserCheck,
+		chromedp.Headless,
+		chromedp.DisableGPU,
+	}
+
+	ctx, cancel := chromedp.NewExecAllocator(context.Background(), opts...)
+	defer cancel()
+
+	// create context
+	ctx, cancel = chromedp.NewContext(
+      ctx,
+		//context.Background(),
+		// chromedp.WithDebugf(log.Printf),
+	)
+	defer cancel()
+
+	// capture screenshot of an element
+	var buf []byte
+   var res string
+
+   log.Print("Navigate")
+	chromedp.Run(ctx, chromedp.Navigate(`https://disneyworld.disney.go.com/dining`))
+   
+   log.Print("Wait")
+	chromedp.Run(ctx, chromedp.WaitVisible(`#pageContainer > div.pepGlobalFooter`, chromedp.ByID))
+
+   log.Print("Type")
+	chromedp.Run(ctx, 
+//      chromedp.Click(`#diningAvailabilityForm-searchDate`, chromedp.ByID),
+//      chromedp.Clear(`#diningAvailabilityForm-searchDate`, chromedp.ByID),
+      chromedp.SetValue(`#diningAvailabilityForm-searchDate`, `11/22/2021`, chromedp.ByID),
+      chromedp.SendKeys(`#searchTime-wrapper > div.select-toggle.hoverable`, `Lunch\r`, chromedp.ByID),
+      chromedp.SendKeys(`#partySize-wrapper > div.select-toggle.hoverable`, `3\r`, chromedp.ByID),
+   )
+
+   log.Print("Click")
+	chromedp.Run(ctx, chromedp.Click(`#dineAvailSearchButton > span > span > span`, chromedp.ByID))
+
+   log.Print("Wait")
+	chromedp.Run(ctx, chromedp.WaitVisible(`#finderList > div > h3:nth-child(2)`, chromedp.ByID))
+
+   log.Print("Write Soruce")
+   chromedp.Run(ctx, 
+      chromedp.ActionFunc(func(ctx context.Context) error {                          
+      node, err := dom.GetDocument().Do(ctx)                                       
+      if err != nil {                                                              
+        return err                                                                 
+      }                                                                            
+      res, err = dom.GetOuterHTML().WithNodeID(node.NodeID).Do(ctx)                
+      return err                                                                   
+    }))
+
+    f, _ := os.Create(`out.html`)
+    defer f.Close()
+    f.WriteString(res)
+
+//   log.Print("ScreenShot")
+//	chromedp.Run(ctx, chromedp.FullScreenshot(&buf, 90))
+
+	if err := ioutil.WriteFile("fullScreenshot.png", buf, 0o644); err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("wrote elementScreenshot.png and fullScreenshot.png")
+}
+
+// elementScreenshot takes a screenshot of a specific element.
+func elementScreenshot(urlstr, sel string, res *[]byte) chromedp.Tasks {
+	return chromedp.Tasks{
+		chromedp.Navigate(urlstr),
+		chromedp.Screenshot(sel, res, chromedp.NodeVisible),
+	}
+}
+
+// fullScreenshot takes a screenshot of the entire browser viewport.
+//
+// Note: chromedp.FullScreenshot overrides the device's emulation settings. Reset
+func fullScreenshot(urlstr string, quality int, res *[]byte) chromedp.Tasks {
+	return chromedp.Tasks{
+		chromedp.Navigate(urlstr),
+		chromedp.FullScreenshot(res, quality),
+	}
+}
