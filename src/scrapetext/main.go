f2c11795a16bdb2c9bf575a2adc15ff106636587
John Orthoefer <john.orthoefer@ll.mit.edu>
2021-09-28 09:54:26 -0400

    parced output

--- src/scrapetext/main.go
+++ src/scrapetext/main.go
@@ -1,33 +1,33 @@
 package main
 
 import (
-  "fmt"
-  "log"
-  "net/http"
-   "os"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
 
-  "github.com/PuerkitoBio/goquery"
+	"github.com/PuerkitoBio/goquery"
 )
 
 func ExampleScrape() {
-  // Request the HTML page.
-  res, err := http.Get("http://metalsucks.net")
-  if err != nil {
-    log.Fatal(err)
-  }
-  defer res.Body.Close()
-  if res.StatusCode != 200 {
-    log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
-  }
+	// Request the HTML page.
+	res, err := http.Get("http://metalsucks.net")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+	if res.StatusCode != 200 {
+		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
+	}
 
-  // Load the HTML document
-  doc, err := goquery.NewDocumentFromReader(res.Body)
-  if err != nil {
-    log.Fatal(err)
-  }
+	// Load the HTML document
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-  // Find the review items
-  doc.Find(".left-content article .post-title").Each(func(i int, s *goquery.Selection) {
+	// Find the review items
+	doc.Find(".left-content article .post-title").Each(func(i int, s *goquery.Selection) {
 		// For each item found, get the title
 		title := s.Find("a").Text()
 		fmt.Printf("Review %d: %s\n", i, title)
@@ -35,30 +35,31 @@ func ExampleScrape() {
 }
 
 func main() {
-   scrape := "./out.html"
-   f, _ := os.Open(scrape)
-   defer f.Close()
+	scrape := "./out.html"
+	f, _ := os.Open(scrape)
+	defer f.Close()
 
-   // Load the HTML document
-   doc, err := goquery.NewDocumentFromReader(f)
-   if err != nil {
-      log.Fatal(err)
-   }
+	// Load the HTML document
+	doc, err := goquery.NewDocumentFromReader(f)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-   // find <li class="card dining show">
-   //  or maybe <div class="cardLink finderCard hasLink" role="link>
-   //  or maybe <div class="cardLinkContainer">
-   // #noAvailability-disneyPicks-default > li:nth-child(1) > div.cardLink.finderCard.hasLink > div.cardLinkContainer
-   // Name  <h2 class="cardName"> NAME </h2>
-   // locate <div class="groupedOffers show">
-   // Time <span class="buttonText"> TIME </span>
-   sel := doc.Find("div.cardLinkContainer")
+	// find <li class="card dining show">
+	//  or maybe <div class="cardLink finderCard hasLink" role="link>
+	//  or maybe <div class="cardLinkContainer">
+	// #noAvailability-disneyPicks-default > li:nth-child(1) > div.cardLink.finderCard.hasLink > div.cardLinkContainer
+	// Name  <h2 class="cardName"> NAME </h2>
+	// locate <div class="groupedOffers show">
+	// Time <span class="buttonText"> TIME </span>
+	sel := doc.Find("div.cardLinkContainer")
 
-   for i := range sel.Nodes {
-   	single := sel.Eq(i)
-      log.Printf("%q", single)
-      for ji, j := range single.Nodes {
-         log.Printf("%d %q", ji, j)
-      }
-   }
+	for i := range sel.Nodes {
+		single := sel.Eq(i)
+		location := single.Find("h2.cardName").Contents().Text()
+		times := single.Find("span.buttonText")
+		for j := range times.Nodes {
+			log.Printf("%s %s", location, times.Eq(j).Contents().Text())
+		}
+	}
 }
