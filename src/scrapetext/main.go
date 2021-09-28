26c46e1884f21fdc2651e0433eb99b45200518e7
John Orthoefer <john.orthoefer@ll.mit.edu>
2021-09-28 11:13:26 -0400

    Parsing!!!

--- src/scrapetext/main.go
+++ src/scrapetext/main.go
@@ -1,38 +1,11 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"net/http"
-	"os"
-
 	"github.com/PuerkitoBio/goquery"
+	"log"
+	"os"
+	"strings"
 )
-
-func ExampleScrape() {
-	// Request the HTML page.
-	res, err := http.Get("http://metalsucks.net")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer res.Body.Close()
-	if res.StatusCode != 200 {
-		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
-	}
-
-	// Load the HTML document
-	doc, err := goquery.NewDocumentFromReader(res.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Find the review items
-	doc.Find(".left-content article .post-title").Each(func(i int, s *goquery.Selection) {
-		// For each item found, get the title
-		title := s.Find("a").Text()
-		fmt.Printf("Review %d: %s\n", i, title)
-	})
-}
 
 func main() {
 	scrape := "./out.html"
@@ -45,6 +18,14 @@ func main() {
 		log.Fatal(err)
 	}
 
+	searchDate, _ := doc.Find("#diningAvailabilityForm-searchDate").Attr("value")
+	searchTime := doc.Find("#searchTime-wrapper > div.select-toggle.hoverable > span > span").Contents().Eq(1).Text()
+	searchSize := doc.Find("#partySize-wrapper > div.select-toggle.hoverable > span > span").Contents().Eq(1).Text()
+
+	log.Printf("Date: %q", searchDate)
+	log.Printf("Time: %s", searchTime)
+	log.Printf("Size: %s", searchSize)
+
 	// find <li class="card dining show">
 	//  or maybe <div class="cardLink finderCard hasLink" role="link>
 	//  or maybe <div class="cardLinkContainer">
@@ -52,14 +33,35 @@ func main() {
 	// Name  <h2 class="cardName"> NAME </h2>
 	// locate <div class="groupedOffers show">
 	// Time <span class="buttonText"> TIME </span>
-	sel := doc.Find("div.cardLinkContainer")
+	//	sel := doc.Find("div.cardLinkContainer")
+	sel := doc.Find("div.cardLink.finderCard.hasLink")
 
 	for i := range sel.Nodes {
 		single := sel.Eq(i)
 		location := single.Find("h2.cardName").Contents().Text()
-		times := single.Find("span.buttonText")
-		for j := range times.Nodes {
-			log.Printf("%s %s", location, times.Eq(j).Contents().Text())
+		u := single.Find("a")
+		url, _ := u.Eq(-1).Attr("href")
+		id, _ := u.Eq(-1).Attr("id")
+		tId := strings.Split(id, ";")
+		id = tId[0]
+		if tId[1] != "entityType=restaurant" {
+			continue
 		}
+		var times []string
+		t := single.Find("span.buttonText")
+		if t.Length() == 0 {
+			continue
+		}
+		t.Each(func(i int, s *goquery.Selection) {
+			times = append(times, s.Text())
+		})
+		if url == "" {
+			log.Printf("!!!! %s %q", location, times)
+		} else {
+			log.Printf("%s - %s %s %q", id, location, url, times)
+		}
+		//		for j := range times.Nodes {
+		//			log.Printf("%s %s", location, times.Eq(j).Contents().Text())
+		//		}
 	}
 }
