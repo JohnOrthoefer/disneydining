b7635949e740948b343f09de75526e2882ea812f
John Orthoefer <john@orthoefer.org>
2023-06-19 19:51:52 -0400

    added FromToday

--- internal/offers/utils.go
+++ internal/offers/utils.go
@@ -2,12 +2,13 @@ package offers
 
 import (
 	"log"
+	"strconv"
 	"strings"
 	"time"
 )
 
-func makeDate(a time.Time)time.Time {
-   return time.Date(a.Year(), a.Month(), a.Day(), 0, 0, 0, 0, disneyTZ)
+func makeDate(a time.Time) time.Time {
+	return time.Date(a.Year(), a.Month(), a.Day(), 0, 0, 0, 0, disneyTZ)
 }
 
 // Join Dining Map, src with Dining Map dst
@@ -19,18 +20,18 @@ func (dst DiningMap) Join(src DiningMap) DiningMap {
 			continue
 		}
 		// just move the times
-      v := dst[idx]
+		v := dst[idx]
 		for _, tent := range ent.Offers {
-         offset := dst[idx].FindOfferByTime(tent.When, tent.Seats)
-         if offset == -1 {
-            v.Offers = append(v.Offers, tent)
-         } else {
-            v.Offers[offset] = tent
-         }
+			offset := dst[idx].FindOfferByTime(tent.When, tent.Seats)
+			if offset == -1 {
+				v.Offers = append(v.Offers, tent)
+			} else {
+				v.Offers[offset] = tent
+			}
 		}
-      dst[idx] = v
+		dst[idx] = v
 	}
-   return dst
+	return dst
 }
 
 // get what time it is at disney world NOW
@@ -40,46 +41,65 @@ func disneyToday() time.Time {
 }
 
 func NormalizeMeal(s string) string {
-   lcs := strings.ToLower(strings.TrimSpace(s))
-   switch lcs {
-   case "breakfast":
-      return "Breakfast"
-   case "brunch":
-      return "Brunch"
-   case "lunch":
-      return "Lunch"
-   case "dinner":
-      return "Dinner"
-   }
-   return lcs
+	lcs := strings.ToLower(strings.TrimSpace(s))
+	switch lcs {
+	case "breakfast":
+		return "Breakfast"
+	case "brunch":
+		return "Brunch"
+	case "lunch":
+		return "Lunch"
+	case "dinner":
+		return "Dinner"
+	}
+	return lcs
 }
 
 func NormalizeDate(b string) time.Time {
-   var rtn time.Time
+	cmd := strings.Fields(strings.ToLower(b))
+	log.Printf("Debug: cmd = %q", cmd)
+	switch cmd[0] {
+	case "today":
+		return disneyToday()
+	case "tomorrow":
+		return disneyToday().Add(time.Hour * 24)
+	case "fromtoday":
+		if len(cmd) > 1 {
+			n, err := strconv.Atoi(cmd[1])
+			if err != nil {
+				break
+			}
+			switch {
+			case n < 0:
+				n = 0
+			case n > 60:
+				n = 60
+			}
 
-   switch b := strings.TrimSpace(strings.ToLower(b)); b {
-   case "today":
-      rtn = disneyToday()
-   case "tomorrow":
-      rtn = disneyToday().Add(time.Hour * 24)
-   default:
-      w, err := time.ParseInLocation("_2 Jan 2006", b, disneyTZ)
-      if err != nil {
-         log.Printf("NormalizeDate error %s", err)
-         rtn = disneyToday()
-      }
-      rtn = w
-   }
-   return rtn
+			rtn := disneyToday().Add(time.Hour * time.Duration(24*n))
+			log.Printf("FromToday = %s", rtn.String())
+			return rtn
+		}
+	default:
+		if len(cmd) > 2 {
+			rtn, err := time.ParseInLocation("_2 Jan 2006", b, disneyTZ)
+			if err != nil {
+				break
+			}
+			return rtn
+		}
+	}
+	log.Printf("NormalizeDate error trouble with %s", b)
+	return disneyToday()
 }
 
 func DateAddTime(d, h string) time.Time {
-   rtn, err := time.ParseInLocation("_2 Jan 2006 15:04", d+" "+h, disneyTZ)
-   if err != nil {
-      log.Printf("DateAddTime error %s", err)
-      return disneyToday()
-   }
-   return rtn
+	rtn, err := time.ParseInLocation("_2 Jan 2006 15:04", d+" "+h, disneyTZ)
+	if err != nil {
+		log.Printf("DateAddTime error %s", err)
+		return disneyToday()
+	}
+	return rtn
 }
 
 // checks that a and b are the same date
@@ -95,8 +115,8 @@ func SameDate(a time.Time, b string) bool {
 
 // Check that string is after today
 func CheckDate(w time.Time) bool {
-	return w.After(disneyToday().AddDate(0, 0, -1)) && 
-      w.Before(disneyToday().AddDate(0,0,adrDays))
+	return w.After(disneyToday().AddDate(0, 0, -1)) &&
+		w.Before(disneyToday().AddDate(0, 0, adrDays+1))
 }
 
 // Match if a any of the substrings, set, appear in string, t.
